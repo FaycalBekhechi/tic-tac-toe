0bf0dfc0f4ec4b43bfb3029dfb610451b5ab8858
Faycal Bekhechi <faycalbekhechi@gmail.com>
2019-03-09 05:38:25 -0800

    reducers tests, fail

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -2,4 +2,4 @@ export const INIT_GAME = 'INIT_GAME';
 export const MOVE_PIECE = 'MOVE_PIECE';
 export const REWIND_MOVE = 'REWIND_MOVE';
 export const RESET_GAME = 'RESET_GAME';
-export const CHECK_COMPLETED_GAME = 'CHECK_COMPLETED_GAME';
+export const COMPLETE_GAME = 'COMPLETE_GAME';
--- src/reducers/__tests__/gameReducer.test.js
+++ src/reducers/__tests__/gameReducer.test.js
@@ -5,62 +5,72 @@ describe('gameReducer', () => {
 	let state;
 	beforeEach(() => {
 		state = {
+			moves: [],
 			board: [],
-			winner: null
+			boardSize: 0,
+			winner: null,
+			draw: false
 		};
 	});
 
 	it('should return the default state', () => {
 		const expected = state;
-		expect(gameReducer(undefined, {})).toEqual(expected);
+		expect(gameReducer(undefined, {})).toMatchObject(expected);
 	});
 
 	it('should return the correct board state when dispatching some actions', () => {
 		const actions = [
 			{},
 			{ type: ActionTypes.INIT_GAME, boardSize: 3 },
-			{ type: ActionTypes.CHECK_COMPLETED_GAME },
 			{ type: ActionTypes.MOVE_PIECE, piece: { player: 1, x: 2, y: 0 } },
-			{ type: ActionTypes.CHECK_COMPLETED_GAME },
 			{ type: ActionTypes.MOVE_PIECE, piece: { player: 2, x: 2, y: 1 } },
-			{ type: ActionTypes.CHECK_COMPLETED_GAME },
 			{ type: ActionTypes.MOVE_PIECE, piece: { player: 1, x: 1, y: 1 } },
-			{ type: ActionTypes.CHECK_COMPLETED_GAME },
-			{ type: ActionTypes.MOVE_PIECE, piece: { player: 2, x: 2, y: 0 } },
-			{ type: ActionTypes.REWIND_MOVE }
+			{ type: ActionTypes.MOVE_PIECE, piece: { player: 2, x: 2, y: 2 } },
+			{ type: ActionTypes.REWIND_MOVE },
+			{ type: ActionTypes.REWIND_MOVE },
+			{ type: ActionTypes.MOVE_PIECE, piece: { player: 1, x: 0, y: 0 } }
 		];
 
 		const actualState = actions.reduce((state, action) => {
 			return gameReducer(state, action);
 		}, undefined);
 		const expected = {
+			moves: [
+				{ player: 1, x: 2, y: 0 },
+				{ player: 2, x: 2, y: 1 },
+				{ player: 1, x: 0, y: 0 }
+			],
 			board: [
-				null, null, { player: 1 },
+				{ player: 1 }, null, { player: 1 },
 				null, null, { player: 2 },
 				null, null, null
-			]
+			],
+			boardSize: 3,
+			winner: null,
+			draw: false
 		};
-		expect(actualState).toEqual(expected);
+		expect(actualState).toMatchObject(expected);
 	});
 
 	it('should return the initial state when resetting the game', () => {
 		const action = { type: ActionTypes.RESET_GAME };
 		const actualState = {
+			moves: [],
 			board: [
-				null, { player: 1 }, null,
-				{ player: 2 }, null, null,
-				{ player: 2 }, { player: 1 }, null
+				null, null, null,
+				null, null, null,
+				null, null, null
 			],
-			winner: null
+			boardSize: 3,
+			winner: null,
+			draw: false
 		};
-		const expected = {
-			board: []
-		};
-		expect(gameReducer(actualState, action)).toEqual(expected);
+		const expected = state;
+		expect(gameReducer(actualState, action)).toMatchObject(expected);
 	});
 
 	it('should return the state with player 1 winning', () => {
-		const action = { type: ActionTypes.CHECK_COMPLETED_GAME };
+		const action = { type: ActionTypes.COMPLETE_GAME, winner: 1, draw: false };
 		const actualState = {
 			board: [
 				{ player: 1 }, { player: 2 }, null,
@@ -77,6 +87,6 @@ describe('gameReducer', () => {
 			],
 			winner: 1
 		};
-		expect(gameReducer(actualState, action)).toEqual(expected);
+		expect(gameReducer(actualState, action)).toMatchObject(expected);
 	});
 });
