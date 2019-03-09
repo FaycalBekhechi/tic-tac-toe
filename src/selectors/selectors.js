8eb882b67d2410a2ca8ce8e5caffee8ca822fd15
Faycal Bekhechi <faycalbekhechi@gmail.com>
2019-03-09 10:28:40 -0800

    current player selector

--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -2,3 +2,10 @@ import { createSelector } from 'reselect';
 
 export const gameStateSelector = state => state.game;
 
+export const currentPlayer = createSelector(
+	[gameStateSelector],
+	gameState => {
+		const { moves } = gameState;
+		return moves.length === 0 ? 1 : (moves[moves.length - 1].player % 2) + 1
+	}
+);
