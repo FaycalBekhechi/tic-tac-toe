c712adf3b14a720b6b7818539d88fa515321e758
Faycal Bekhechi <faycalbekhechi@gmail.com>
2019-03-08 17:56:02 -0800

    pass App test

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import logo from 'images/logo.svg';
+import React from 'react';
 import './App.css';
+import BoardHeader from "components/BoardHeader";
+import Board from "./Board";
+import BoardActions from "components/BoardActions";
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <BoardHeader/>
+          <Board/>
+          <BoardActions/>
+        </div>
     );
   }
 }
