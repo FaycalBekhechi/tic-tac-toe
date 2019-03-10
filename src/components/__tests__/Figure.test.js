c542154904e21cf08f576fd70e484c99a314e825
Faycal Bekhechi <faycalbekhechi@gmail.com>
2019-03-10 08:53:08 -0700

    figure custom classname test

--- src/components/__tests__/Figure.test.js
+++ src/components/__tests__/Figure.test.js
@@ -7,4 +7,9 @@ describe('<Figure/>', () => {
 		const renderedComponent = mount(<Figure player={1}/>);
 		expect(renderedComponent.find('span')).toHaveLength(1);
 	});
+
+	it('should render the element with a classname', () => {
+		const renderedComponent = mount(<Figure player={2} className="figure-appear"/>);
+		expect(renderedComponent.find('span.figure-appear')).toHaveLength(1);
+	})
 });
