e4d295ad1818df3a43b90b880aa6bfb1b8487ced
Faycal Bekhechi <faycalbekhechi@gmail.com>
2019-03-10 12:22:54 -0700

    score panel current player test

--- src/components/__tests__/ScorePanel.test.js
+++ src/components/__tests__/ScorePanel.test.js
@@ -8,4 +8,8 @@ describe('<ScorePanel/>', () => {
 		const renderedComponent = shallow(<ScorePanel player1Wins={88} player2Wins={12} draw={0}/>);
 		expect(renderedComponent.find('.score')).toHaveLength(3);
 	});
+	it('should render the cursor on the current player', () => {
+		const renderedComponent = shallow(<ScorePanel player1Wins={88} player2Wins={12} draw={0} currentPlayer={2}/>);
+		expect(renderedComponent.find('.player-score-cursor').at(2).hasClass('player-score-cursor-show')).toBe(true);
+	});
 });
